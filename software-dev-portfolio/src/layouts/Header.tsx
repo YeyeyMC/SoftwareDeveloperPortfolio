008007620d75652b56798ade35ab42fc8f14458c
Yeison Munoz <yeyeygameplays@gmail.com>
2026-07-22 15:13:55 -0700

    first commit

---
+++ software-dev-portfolio/src/layouts/Header.tsx
@@ -0,0 +1,18 @@
+﻿import React from 'react';
+
+const Header = () => {
+    return (
+        <header>
+            <nav>
+                <ul>
+                    <li><a href="#">About</a></li>
+                    <li><a href="#">Education</a></li>
+                    <li><a href="#">Experience</a></li>
+                    <li><a href="#">Skills</a></li>
+                    <li><a href="#">Project</a></li>
+                    <li><a href="#">Contact</a></li>
+                </ul>
+            </nav>
+        </header>
+    )
+}
